1b83e2c0c146d7792f1da983e2032ef7019e8471
luosai <luosai19910103@163.com>
2015-09-28 19:51:51 +0800

    Initial Commit

---
+++ swiftdemoTests/swiftdemoTests.swift
@@ -0,0 +1,36 @@
+//
+//  swiftdemoTests.swift
+//  swiftdemoTests
+//
+//  Created by luosai19910103@163.com on 15/9/28.
+//  Copyright © 2015年 wuyouyunche. All rights reserved.
+//
+
+import XCTest
+@testable import swiftdemo
+
+class swiftdemoTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
